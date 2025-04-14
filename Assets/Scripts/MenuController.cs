d2ecaf905cadcfae8068c0e9f58790c6734b80d6
Blujuse-Will <wlhps@outlook.com>
2025-04-14 23:00:38 +0100

    Updated Font & Added Help Button

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public GameObject loadingScreen;
+    public Image loadingBar;
+
     // Load back to the menu
     public void BackToTheMenu()
     {
@@ -27,5 +31,34 @@ public class MenuController : MonoBehaviour
     public void LoadTrainYard()
     {
         SceneManager.LoadScene("TrainYard");
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        if (loadingScreen == null)
+        {
+            Debug.LogError("Loading screen not assigned.");
+            return;
+        }
+        else
+        {
+            StartCoroutine(LoadSceneAsync(sceneIndex));
+        }
+    }
+
+    IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneIndex);
+
+        loadingScreen.SetActive(true);
+
+        while (!asyncLoad.isDone)
+        {
+            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
+
+            loadingBar.fillAmount = progress;
+
+            yield return null;
+        }
     }
 }
